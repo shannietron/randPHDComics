b4d351567cebc70b074374ba971e50d1d6b36878
Shanmugam MPl <git@shan.mp>
2017-04-19 03:32:28 -0400

    embed phdComic in an iframe, add rand button

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-var maxID = 1940;
-var randID = Math.ceil((Math.random() * maxID));
-var randComicID = ("0000" + randID).substr(-4,4);
-window.location = 'http://phdcomics.com/comics/archive.php?comicid=' + randComicID;
+window.addEventListener("load", function(){
+
+var randFrame = document.getElementById("frame");
+var randButton = document.getElementById("randButton");
+
+function genRandomUrl(randFrame) {
+	var maxID = 1942;
+	var randID = Math.ceil((Math.random() * maxID));
+	var randComicID = ("0000" + randID).substr(-4,4);
+	randFrame.src = 'http://phdcomics.com/comics/archive.php?comicid=' + randComicID;
+}
+
+randButton.addEventListener("click",function(){
+	genRandomUrl(randFrame);
+});
+genRandomUrl(randFrame);
+});
