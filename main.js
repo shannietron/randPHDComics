3545dfefdfe1d69d5cfed46aab75388d8957c256
Shanmugam MPl <git@shan.mp>
2017-05-09 00:55:52 -0400

    grep for archive_print.php?comicid=

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+var phdstr = /archive_print.php\?comicid=..../;
+
 window.addEventListener("load", function(){
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function getMaxID(data){
+	found = data.match(phdstr);
+}
+
+httpGetAsync("https://crossorigin.me/http://phdcomics.com/", getMaxID);
+
 
 var randFrame = document.getElementById("frame");
 var randButton = document.getElementById("randButton");
